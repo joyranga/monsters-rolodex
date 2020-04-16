102244e3938b0009873498c8e407e696a030b49d
Ajoy Ranga <RANGAJY@LC02VL1BEHTDD>
2020-04-16 00:42:30 -0400

    init add of project files

---
+++ build/precache-manifest.ed6cc0494983193e26addabcc5e5c877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c89bc3f8095ee42d7b57cadf3b191d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5eb95106a48ff77191b0",
+    "url": "/monsters-rolodex/static/css/main.bbee3475.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb95106a48ff77191b0",
+    "url": "/monsters-rolodex/static/js/main.cd97b363.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
